209d5785b3532eae97f5ce9e48e56a0ff22916b7
fajar purnomo aji <purnomoajifajar2@gmail.com>
2025-05-24 22:01:23 +0700

    menambah sidebar2 untuk home

---
+++ application/views/template/sidebar2.php
@@ -0,0 +1,27 @@
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm p-4 mb-4 bg-white">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" style="font-size: 30px;" href="<?= base_url('') ?>"><strong>S</strong>ikar<strong>KK</strong></a>
+
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                </ul>
+                <ul class="navbar-nav" style="font-size: 20px;">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="<?= base_url() ?>">Beranda <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= base_url('auth') ?>">Masuk</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <!-- Main content -->
+        <div class="content">
+            <div class="container-fluid">
